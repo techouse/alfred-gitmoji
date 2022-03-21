9627e3539745f3e7d4b043fae45078015affea74
Klemen Tušar <techouse@gmail.com>
2022-03-21 00:59:51 +0000

    :wrench: Update build script

--- build.sh
+++ build.sh
@@ -9,4 +9,11 @@ fi
 
 mkdir -p build/dist build/debug_info
 cp -r info.plist assets/* LICENSE README.md build/dist
+
+if command -v dart-pubspec-licenses-lite; then
+  dart-pubspec-licenses-lite --pubspec-lock pubspec.lock --output build/dist/OSS_LICENSES.txt
+else
+  echo 'Info: Unable to generate OSS_LICENSES.txt. Please install https://github.com/techouse/flutter_oss_licenses'
+fi
+
 dart compile exe bin/main.dart -o build/dist/gm -S build/debug_info/gm
