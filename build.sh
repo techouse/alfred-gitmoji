82748a4a3755f9cb8ebc8b69f93d2e7a87dfe642
Klemen Tušar <techouse@gmail.com>
2022-03-21 21:50:42 +0000

    :wrench: Update build script

--- build.sh
+++ build.sh
@@ -1,4 +1,23 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+
+# Function to build header
+oss_header() {
+  first=$(printf '%.s#' {1..78})
+  second=$(
+    printf '##'
+    printf '%.s ' {1..74}
+    printf '##'
+  )
+  third=$(
+    printf '##'
+    printf '%.s ' {1..27}
+    printf 'OPEN-SOURCE LICENSES'
+    printf '%.s ' {1..27}
+    printf '##'
+  )
+  printf "\n\n%s\n%s\n%s\n%s\n%s\n\n" "$first" "$second" "$third" "$second" "$first"
+}
+
 if [ -d "build/dist" ]; then
   rm -rf build/dist
 fi
@@ -11,7 +30,8 @@ mkdir -p build/dist build/debug_info
 cp -r info.plist assets/* LICENSE README.md build/dist
 
 if command -v dart-pubspec-licenses-lite; then
-  dart-pubspec-licenses-lite --pubspec-lock pubspec.lock --output build/dist/OSS_LICENSES.txt
+  oss_header >>build/dist/LICENSE
+  dart-pubspec-licenses-lite --pubspec-lock pubspec.lock >>build/dist/LICENSE
 else
   echo 'Info: Unable to generate OSS_LICENSES.txt. Please install https://github.com/techouse/dart_pubspec_licenses_lite'
 fi
