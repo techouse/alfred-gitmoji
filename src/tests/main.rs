c94e0a8b403aacc3c9c7595b954e625e8afaf7b5
Klemen Tusar <techouse@gmail.com>
2026-08-19 08:43:33 +0100

    fix: make cached Gitmoji items durable

--- src/tests/main.rs
+++ src/tests/main.rs
@@ -5,12 +5,26 @@ use anyhow::anyhow;
 
 use super::*;
 
+fn cached_search_result() -> SearchResult {
+    SearchResult {
+        object_id: "bug".to_owned(),
+        emoji: "🐛".to_owned(),
+        entity: "&#x1f41b;".to_owned(),
+        code: ":bug:".to_owned(),
+        description: "Fix a bug".to_owned(),
+        name: "bug".to_owned(),
+        semver: Some("patch".to_owned()),
+    }
+}
+
 #[test]
 fn file_cache_hit_bypasses_algolia_and_image_resolution() -> Result<()> {
     let directory = tempfile::tempdir()?;
     let mut cached_workflow = Workflow::with_file_cache(FileCache::with_path(directory.path()));
     cached_workflow.set_cache_key(Some("cached-query"));
-    cached_workflow.add_item(Item::new("cached result"))?;
+    let cached_item =
+        items_from_results(&[cached_search_result()], &[None::<&std::path::Path>])?.remove(0);
+    cached_workflow.add_item(cached_item.clone())?;
 
     let mut workflow = Workflow::with_file_cache(FileCache::with_path(directory.path()));
     let algolia_calls = Cell::new(0);
@@ -35,7 +49,7 @@ fn file_cache_hit_bypasses_algolia_and_image_resolution() -> Result<()> {
 
     assert_eq!(algolia_calls.get(), 0);
     assert_eq!(image_resolution_calls.get(), 0);
-    assert_eq!(workflow.get_items()?.len(), 1);
+    assert_eq!(workflow.get_items()?.items(), &[cached_item]);
 
     Ok(())
 }
